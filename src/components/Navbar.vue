68bed3f0f6840a366ec31e337bfcd1cb30f03667
sabilimuhammad <m.afriansabili@gmail.com>
2020-02-07 01:59:53 +0700

    origin master

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <div>
+    <p v-if="isShow">{{car.type}}, {{car.color}}</p>
+  </div>
+  <button type="button" @click="isShow = true"> Change Name!</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message : 'who is this?',
+            name : 'bili',
+            isShow: false,
+            car: {
+            type: 'huracan',
+            color: 'black',
+        },
+    };
+    },
+    methods: {
+        changeName(){
+            this.isShow = true;
+        },
+}
+}
+</script>
+
+<style>
+</style>
