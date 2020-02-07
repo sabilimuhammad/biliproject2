4fbff397bd7579afc08b247aa4059793f57bcb08
sabilimuhammad <m.afriansabili@gmail.com>
2020-02-08 01:19:16 +0700

    updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,11 @@
       <div>
     <p v-if="isShow">{{car.type}}, {{car.color}}</p>
   </div>
-  <button type="button" @click="isShow = true"> Change Name!</button>
+<!-- <img :src="imageUrl" alt="alt">
+  <button type="button" @click="changeimg"> Change Button </button> -->
+  <ul>
+      <li v-for="name in nameList" :key="name.name">{{name.name}}</li> 
+  </ul>
   </div>
 </template>
 
@@ -11,18 +15,28 @@
 export default {
     data(){
         return {
-            message : 'who is this?',
-            name : 'bili',
-            isShow: false,
+            // message : 'who is this?',
+            // name : 'bili',
+            image1: 'https://www.carlogos.org/logo/Ferrari-logo-2560x1440.png',
             car: {
-            type: 'huracan',
-            color: 'black',
+            // type: 'huracan',
+            // color: 'black',
         },
+        imageUrl : 'https://www.carlogos.org/logo/Lamborghini-logo-1920x1080.png',
+        alt : 'alt',
+        nameList: [
+            {
+            name : 'Laferrari',
+        },
+        {
+            name : 'Aventador',
+        },
+        ]
     };
     },
     methods: {
-        changeName(){
-            this.isShow = true;
+        changeimg(){
+            this.image1 = this.changeimg;
         },
 }
 }
